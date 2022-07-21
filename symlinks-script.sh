349106c796a5ae6cdd5bd3a72582d959ab33a745
Carlos Gonzalez <carlos.gonzalez@thoughtworks.com>
2022-07-21 09:31:23 -0400

    Created symlinks script

---
+++ symlinks-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/lvim/config.lua ~/.config/lvim/config.lua
+ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#   ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+#   ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json 
+
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#         # Mac OSX
+#   ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+#   ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+# fi
+
+
