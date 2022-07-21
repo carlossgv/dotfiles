798782190d117d50ac532c43c484375fa3930aa0
Carlos Gonzalez <carlos.gonzalez@thoughtworks.com>
2022-07-21 09:52:47 -0400

    Fixed symlinks to take in consideration OS

--- symlinks-script.sh
+++ symlinks-script.sh
@@ -4,14 +4,12 @@ ln -sf ~/dotfiles/lvim/config.lua ~/.config/lvim/config.lua
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#   ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
-#   ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ln -s ~/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+  ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
-# elif [[ "$OSTYPE" == "darwin"* ]]; then
-#         # Mac OSX
-#   ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-#   ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-# fi
-
-
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+  ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+  ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+fi
