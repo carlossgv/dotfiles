18ee2d1548748927fad90084d97cb9cf6d38234b
Carlos Gonzalez <carlos.gonzalez@thoughtworks.com>
2022-07-27 18:50:00 -0400

    Added everything to a single lua file for the time beign

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,5 +1,23 @@
-require('set')
+-- require("lua.set")
 print("Loading init.lua");
+
+-- SETTERS
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.smartindent = true
+-- vim.o.nowrap = true
+vim.o.scrolloff = 8
+vim.o.spell = true
+vim.o.hidden = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- vim.o.signcolumn = true
+vim.o.timeoutlen = 500
+
 -- 	" PLUGINS
 -- 	call plug#begin()
 
