5dc40cb11ab1bf1872e0a2073a09849fed23e1b6
Carlos Gonzalez <carlos.gonzalez@thoughtworks.com>
2022-07-30 21:25:10 -0400

    Formatting

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,7 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "everforest"
-vim.opt.clipboard="unnamedplus"
+vim.opt.clipboard = "unnamedplus"
 vim.opt.relativenumber = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
--- symlinks-script.sh
+++ symlinks-script.sh
@@ -10,7 +10,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   ln -s ~/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
+  # Mac OSX
   ln -s ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
   ln -s ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 fi
